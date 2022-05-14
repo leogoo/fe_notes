9ba2ddf6c13f088d776edefe830d9a6bb8da4c65
gaolee <1798476449@qq.com>
2022-05-14 17:48:46 +0800

    自动部署测试

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   base: '/fe_notes/',
   themeConfig: {
     nav: [
-      { text: '首页', link: '/css/index' },
+      { text: '首页', link: '/' },
       { text: 'demo', link: '/demo/index'},
       {
         text: 'leo',
---
+++ docs/node/index.md
@@ -0,0 +1,5 @@
+### 基础
+1. 直接安装cnpm设置淘宝源，避免内部私域的频繁切换
+    ```js
+    npm install -g cnpm --registry=https://registry.npm.taobao.org
+    ```
